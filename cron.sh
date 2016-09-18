b4d80910543885e036640e2241a2c745defd91ae
zonesan <san@zoobook.org>
2016-09-18 10:30:10 +0800

    2016-09-18-10:27:33.log generated.

--- cron.sh
+++ cron.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+WORKDIR=`pwd`
 LOGFILENAME=`date  +%Y-%m-%d-%H:%M:%S.log`
 
 git pull
 
 #./sync-with-github-and-gitlab.sh > ./logs/`date  +%Y-%m-%d-%H:%M:%S.log` 2>&1
-./sync-with-github-and-gitlab.sh > ./logs/${LOGFILENAME} 2>&1
+${WORKDIR}/sync-with-github-and-gitlab.sh > ./logs/${LOGFILENAME} 2>&1
 
 git add .
 
